2b574f4d53ddd7970f5854607476741171014892
PreCho01 <precho01@outlook.com>
2025-04-30 09:50:39 +0530

    added palindrom concept

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -1,11 +1,15 @@
-﻿namespace GitPractice
+﻿using Helper;
+
+namespace GitPractice
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Git Branching Practice");
-           
+            Console.WriteLine("\n-- Palindrome Check --");
+            Console.WriteLine("Is 'madam' a palindrome? " + PalindromeHelper.IsPalindrome("madam"));
+
         }
     }
 }
---
+++ Helper/PalindromHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Helper
+{
+    public static class PalindromeHelper
+    {
+        public static bool IsPalindrome(string word)
+        {
+            string reversed = new string(word.Reverse().ToArray());
+            return word.ToLower() == reversed.ToLower();
+        }
+    }
+}
