33207f6178afe56820c83330e8374062244f0991
PreCho01 <precho01@outlook.com>
2025-04-30 08:47:43 +0530

    Add project files.

---
+++ GitPractice/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GitPractice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+           
+        }
+    }
+}
