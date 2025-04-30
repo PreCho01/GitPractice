70c03c4ac7ff64dab8d29b13eec41530c4ba8275
PreCho01 <precho01@outlook.com>
2025-04-30 09:18:33 +0530

    first commit

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace GitPractice
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Git Branching Practice");
            
         }
     }
